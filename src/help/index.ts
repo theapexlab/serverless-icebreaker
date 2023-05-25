7981d51771b0d6272947e209e0a0586db6e9a89c
Attila Zsolt Danku <attila@apexlab.io>
2023-05-25 09:59:50 +0200

    feat: extend help list

--- src/help/index.ts
+++ src/help/index.ts
@@ -1,11 +1,20 @@
 import { CommandLineArgs } from "../utils/get-command-line-args";
 
 export const showHelp = () => {
+  const name = CommandLineArgs.name;
+  const fileSize = CommandLineArgs.fileSize;
+  const showOnlyErrors = CommandLineArgs.showOnlyErrors;
+
   console.log("Help is on the way! \n");
-  console.log(`Command line parameters: \n`);
-  console.log(`  --${CommandLineArgs.name}: filter by lambda name (startWith / string)\n
-  --${CommandLineArgs.fileSize}: overwrite the default warning threshold size (number MB)\n
-  --${CommandLineArgs.showOnlyErrors}: show only errors (boolean)\n
+  console.log(`Parameters: \n`);
+  console.log(`  --${name}: filter by lambda name (startWith / string)\n
+  --${fileSize}: overwrite the default warning threshold size (number MB)\n
+  --${showOnlyErrors}: show only errors (boolean)\n
   --${CommandLineArgs.help}:  Show this help (boolean)\n`);
+  console.log(`Examples: \n`);
+  console.log(`  yarn cst\n
+  yarn cst --${name}=get\n
+  yarn cst --${name}=get --${fileSize}=30\n
+  yarn cst --${name}=get --${fileSize}=30 --${showOnlyErrors}\n`);
   process.exit(0);
 };
