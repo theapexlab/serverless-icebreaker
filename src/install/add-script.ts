b2e0b833ed2884725b195fbb68626c53efaf5408
Attila Zsolt Danku <attila@apexlab.io>
2023-05-25 14:27:21 +0200

    fix: add and remove script path

--- src/install/add-script.ts
+++ src/install/add-script.ts
@@ -3,21 +3,19 @@ import path from "path";
 
 const cstScript = "node ./node_modules/cold-start-tool/dist";
 
-export const addScript = () => {
-  // Get the root directory of the project
-  const projectRoot = path.resolve(__dirname, "../../");
+// Get the root directory of the project
+const projectRoot = path.resolve(__dirname, "../../../../../");
 
-  // Read the package.json file of the project
-  const projectPackageJsonPath = path.join(projectRoot, "package.json");
-  const packageJson = require(projectPackageJsonPath);
+// Read the package.json file of the project
+const projectPackageJsonPath = path.join(projectRoot, "package.json");
+const packageJson = require(projectPackageJsonPath);
 
-  // Add the desired command to the scripts object
-  packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.cst = cstScript;
+// Add the desired command to the scripts object
+packageJson.scripts = packageJson.scripts || {};
+packageJson.scripts.cst = cstScript;
 
-  // Update the package.json file of the project
-  fs.writeFileSync(
-    projectPackageJsonPath,
-    JSON.stringify(packageJson, null, 2)
-  );
-};
+// Update the package.json file of the project
+fs.writeFileSync(
+  projectPackageJsonPath,
+  JSON.stringify(packageJson, null, 2)
+);
--- src/install/remove-script.ts
+++ src/install/remove-script.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 // Get the root directory of the project
-const projectRoot = path.resolve(__dirname, "../../");
+const projectRoot = path.resolve(__dirname, "../../../../../");
 
 // Path to the package.json file
 const packageJsonPath = path.resolve(projectRoot, "package.json");
