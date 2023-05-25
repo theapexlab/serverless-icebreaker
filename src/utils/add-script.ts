3827c64ae887804901523a8306ec754c1a68c34c
Attila Zsolt Danku <attila@apexlab.io>
2023-05-25 08:28:44 +0200

    feat: add cst script after the package was installed automatically

---
+++ src/utils/add-script.ts
@@ -0,0 +1,16 @@
+import fs from "fs";
+import path from "path";
+
+// Get the root directory of the project
+const projectRoot = path.resolve(__dirname, "../../");
+
+// Read the package.json file of the project
+const projectPackageJsonPath = path.join(projectRoot, "package.json");
+const packageJson = require(projectPackageJsonPath);
+
+// Add the desired command to the scripts object
+packageJson.scripts = packageJson.scripts || {};
+packageJson.scripts.cst = "npx ts-node ./node_modules/cold-start-tool";
+
+// Update the package.json file of the project
+fs.writeFileSync(projectPackageJsonPath, JSON.stringify(packageJson, null, 2));
