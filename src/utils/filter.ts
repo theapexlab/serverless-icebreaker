38e88d667c2ec48bb35c07537d918a009a1142e8
zolle <debrecenizoltan19@gmail.com>
2023-05-31 11:02:58 +0200

    fix: make regex more readable

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,4 +1,7 @@
 import { config } from "../..";
 
 export const filterByNameAndExtension = (fileName: string) =>
-  fileName.startsWith(config.filterByName) && /\.(js|mjs)$/i.test(fileName);
+  fileName.startsWith(config.filterByName) && filterByExtension(fileName);
+
+// Filter for .js and .mjs files
+const filterByExtension = (fileName: string) => /\.(js|mjs)$/i.test(fileName);
