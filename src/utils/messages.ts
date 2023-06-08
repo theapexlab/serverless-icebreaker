655497e5dfb56d0c172b17f085b788b5d728e4bb
zolle <debrecenizoltan19@gmail.com>
2023-06-08 13:18:27 +0200

    fix: message

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,4 +1,4 @@
 export enum Messages {
   PATH_ERROR = "No lambdas found, please run a build first or check your buildPath",
-  CONFIG_CREATED = 'A configuration file has been created in the project root as "cst-config.json". You can modify it at any time or use "cst --init" to reconfigure.\nPress any key to continue...',
+  CONFIG_CREATED = 'A configuration file has been created in the project root as "cst-config.json". You can modify it at any time or use "npx cst --init" to reconfigure.\nPress any key to continue...',
 }
