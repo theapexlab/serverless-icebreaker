31da7f6f1a8a968a103f035440de2fd660236aab
Attila Zsolt Danku <attila@apexlab.io>
2023-06-15 11:17:51 +0200

    feat: make a predicton about possible cold start duration

--- src/output/get-lambda-details.ts
+++ src/output/get-lambda-details.ts
@@ -1,8 +1,14 @@
+import { predictColdStartDuration } from "./get-cold-start-prediction";
+
 export const getLambdaDetails = (
   lambdaSize: number,
   modules: number,
   frequentModules: string
-) =>
-  `  Lambda size: ${lambdaSize} MB
+) => {
+  const coldStartDuration = predictColdStartDuration(lambdaSize);
+
+  return `  Lambda size: ${lambdaSize} MB
    Imported modules: ${modules}
+   Possible Cold Start Duration: ~${coldStartDuration} ms
    Most frequent modules: ${frequentModules}\n`;
+};
