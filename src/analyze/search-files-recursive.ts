e4a2e7db2bd63e6feba44ee3d6098f0a3af047b5
zolle <debrecenizoltan19@gmail.com>
2023-06-24 14:40:37 +0200

    feat: cleaner recursion

--- src/analyze/search-files-recursive.ts
+++ src/analyze/search-files-recursive.ts
@@ -1,4 +1,4 @@
-import { readdirSync, statSync } from "fs";
+import { existsSync, readdirSync, statSync } from "fs";
 import path from "path";
 import { filterByExtension } from "../utils/filter";
 
@@ -11,12 +11,9 @@ export const searchFilesRecursive = (
   for (const file of files) {
     const filePath = path.join(directoryPath, file);
 
-    let stats;
-    try {
-      stats = statSync(filePath);
-    } catch (e) {
-      continue;
-    }
+    const stats = existsSync(filePath) && statSync(filePath);
+
+    if (!stats) continue;
 
     if (stats?.isDirectory()) {
       const nestedFiles = searchFilesRecursive(filePath, extensions);
