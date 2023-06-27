2e9e103b0869ad2f3424c8c25b3a52ac28de0c49
Attila Zsolt Danku <attila@apexlab.io>
2023-06-27 16:26:49 +0200

    feat: refactor and use enum to store lambda sizes with durations

--- src/output/get-cold-start-prediction.ts
+++ src/output/get-cold-start-prediction.ts
@@ -1,55 +1,31 @@
-enum SizesOfLambdas {
-  "1MB" = 1,
-  "19.6MB" = 19.6,
-  "30.2MB" = 30.2,
-  "52.8MB" = 52.8
-}
-
-enum ColdStartDurationsInMS {
-  "150MS" = 150,
-  "692MS" = 692,
-  "1716MS" = 1716,
-  "2515MS" = 2515
+enum SizesOfLambdasWithDurations {
+  "1MB" = "150MS",
+  "19.6MB" = "692MS",
+  "30.2MB" = "1716MS",
+  "52.8MB" = "2515MS"
 }
 
 export const getColdStartPrediction = (lambdaSize: number) => {
-  if (lambdaSize <= SizesOfLambdas["1MB"]) {
-    return ColdStartDurationsInMS["150MS"];
+  for (const [size, duration] of Object.entries(SizesOfLambdasWithDurations)) {
+    const sizeValue = parseFloat(size);
+    const durationValue = parseFloat(duration);
+
+    if (lambdaSize <= sizeValue) {
+      return calculateColdStartPrediction(lambdaSize, durationValue, sizeValue);
+    }
   }
 
-  if (lambdaSize <= SizesOfLambdas["19.6MB"]) {
-    return calculateColdStartPrediction(
-      lambdaSize,
-      ColdStartDurationsInMS["692MS"],
-      SizesOfLambdas["19.6MB"],
-      ColdStartDurationsInMS["150MS"]
-    );
-  }
-
-  if (lambdaSize <= SizesOfLambdas["30.2MB"]) {
-    return calculateColdStartPrediction(
-      lambdaSize,
-      ColdStartDurationsInMS["1716MS"],
-      SizesOfLambdas["30.2MB"],
-      ColdStartDurationsInMS["692MS"]
-    );
-  }
-
-  return calculateColdStartPrediction(
-    lambdaSize,
-    ColdStartDurationsInMS["2515MS"],
-    SizesOfLambdas["52.8MB"],
-    ColdStartDurationsInMS["1716MS"]
+  const largestSize = parseFloat(
+    Object.keys(SizesOfLambdasWithDurations).pop()!
   );
+  const largestDuration = parseFloat(
+    Object.values(SizesOfLambdasWithDurations).pop()!
+  );
+  return calculateColdStartPrediction(lambdaSize, largestSize, largestDuration);
 };
 
 const calculateColdStartPrediction = (
   lambdaSize: number,
   coldStartDurationMS: number,
-  exampleLambdaMB: number,
-  fallbackMS: number
-) =>
-  Math.max(
-    Math.round(lambdaSize * (coldStartDurationMS / exampleLambdaMB)),
-    fallbackMS
-  );
+  exampleLambdaMB: number
+) => Math.round(lambdaSize * (coldStartDurationMS / exampleLambdaMB));
