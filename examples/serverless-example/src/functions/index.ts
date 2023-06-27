684fee8201ae516cf45e3974362f86dfb9979ed9
zolle <debrecenizoltan19@gmail.com>
2023-06-27 12:52:21 +0200

    fix: remove examples

--- examples/serverless-example/src/functions/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as simpleLambda } from "./simple-lambda";
-export { default as getLongColdStart } from "./get-long-cold-start";
-export { default as optimized } from "./optimized";
-export { default as unoptimized } from "./unoptimized";
--- examples/sst-example/packages/functions/src/simple-lambda.ts
+++
@@ -1,7 +0,0 @@
-// basic hello world function with small file size and fast cold start
-export async function handler() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("hello world")
-  };
-}
