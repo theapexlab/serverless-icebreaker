f8ab6621a85fa2bc03fc04531fc2b4db907a93ac
zolle <debrecenizoltan19@gmail.com>
2023-06-28 17:26:18 +0200

    fix: prettier constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
 import type { Configuration } from "../types";
 
 export const SEARCH_TERM = "// node_modules/";
-
 export const NODE_MODULES = "node_modules";
 export const DISSALLOWED_FILE_NAMES = ["handler.js", "handler.mjs"];
 export const BUILT_FILE_EXTENSIONS = ["js", "mjs"];
 export const COMPRESSED_FILE_EXTENSIONS = ["zip"];
 export const MIXPANEL_COLLECTION_NAME = "sib-run";
+
 export const defaultConfig: Configuration = {
   buildPath: ".sst/artifacts",
   errorThresholdMB: 10,
