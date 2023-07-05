6e539a3003cc79ad95d8c9a572949a95565d37e8
Attila Zsolt Danku <attila@apexlab.io>
2023-07-05 08:59:23 +0200

    feat: separate and use only get an initial answer

--- src/user-input/init-handler.ts
+++ src/user-input/init-handler.ts
@@ -1,11 +1,24 @@
 import { suggestedInit } from "./suggested-init";
 import { init } from "./init";
-import { isSuggestedInit } from "./is-suggested-init";
+import { getInitialAnswer } from "./get-initial-answer";
+import { ConfigurationMethod } from "../types";
+
+const isSuggestedInit = (init: string) => init !== ConfigurationMethod.CUSTOM;
+
+const findConfigurationMethodKeyByValue = (value: string) => {
+  const enumKeys = Object.keys(ConfigurationMethod);
+  const key = enumKeys.find(key => ConfigurationMethod[key as keyof typeof ConfigurationMethod] === value);
+  return key || enumKeys[0];
+};
 
 export const initHandler = async () => {
-  const { isSuggested, framework } = await isSuggestedInit();
+  const initialAnswer = await getInitialAnswer();
+  const isSuggested = isSuggestedInit(initialAnswer);
+
   if (isSuggested) {
+    const framework = findConfigurationMethodKeyByValue(initialAnswer);
     return await suggestedInit(framework);
   }
+
   return await init();
 };
