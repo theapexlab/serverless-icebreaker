fd135d023ff87776abf699c319d9f0311cb8394b
Attila Zsolt Danku <attila@apexlab.io>
2023-07-11 10:45:29 +0200

    fix: CONFIG_CREATED message

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,6 +1,6 @@
 export enum Messages {
   PATH_ERROR = "No lambdas found, please run a build first or check your buildPath",
-  CONFIG_CREATED = 'A configuration file has been created in the project root as "sib-config.json". You can modify it at any time or use "sib --init" to reconfigure.\nPress any key to continue...',
+  CONFIG_CREATED = 'A configuration file has been created in the project root as "sib-config.json". You can modify it at any time or use "npx sib --init" to reconfigure.\nPress any key to continue...',
   ERROR_THRESHOLD_EXCEEDED = "Serverless Icebreaker WARNING: threshold set in sib-config.json is exceeded by the following lambdas:\n",
   COMPRESSED_FILE_FOUND = "Compressed files found, decompressing..."
 }
