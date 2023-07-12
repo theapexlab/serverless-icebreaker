5bb47fe2001f6ac7e63997f40f0304ac08f27f13
Attila Zsolt Danku <attila@apexlab.io>
2023-07-12 14:26:19 +0200

    feat: rename analytics folder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { main } from "./analitics";
+import { main } from "./analytics";
 import { getCommandLineArgs } from "./user-input/get-command-line-args";
 
 export const projectRoot = process.cwd();
